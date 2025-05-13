7b0c5102b186583ba83d4332b8e786cb6e76a2dc
drharshbanarwal <drharshbanarwal@gmail.com>
2025-05-13 18:33:51 +0530

    Add files via upload

---
+++ SQL DAY 1 (13th may).sql
@@ -0,0 +1,16 @@
+select database();
+
+use sakila;
+
+select database();
+
+show databases;
+show tables;
+
+select * from actor;
+
+select actor_id, first_name from actor;
+
+select actor_id, first_name, last_name from actor;
+
+describe actor;
